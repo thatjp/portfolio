085fb46bdec679c8efa3d2729f740039c8c696ec
JP Harris <jpharris.contact@gmail.com>
2023-05-17 15:19:52 -0400

    Comment out css import for gatsby cloud error test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,15 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
+// /**
+//  * Implement Gatsby's Browser APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
+//  */
 
-// You can delete this file if you're not using it
+// // You can delete this file if you're not using it
 
-import React from 'react';
-import RootElement from './src/components/RootElement';
-import './src/styles/global.css'
+// import React from 'react';
+// import RootElement from './src/components/RootElement';
+// import './src/styles/global.css'
 
-export const wrapRootElement = ({ element }) => {
-  return <RootElement>{element}</RootElement>;
-};
+// export const wrapRootElement = ({ element }) => {
+//   return <RootElement>{element}</RootElement>;
+// };
