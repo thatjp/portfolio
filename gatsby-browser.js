0b06087c4c86d4a682a06f52a70c225e178fdbf1
JP Harris <jpharris.contact@gmail.com>
2023-05-17 17:17:32 -0400

    updates css imports for err test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,7 @@
 
 // import React from 'react';
 // import RootElement from './src/components/RootElement';
-import './src/styles/global.css'
+// import './src/styles/global.css'
 
 // export const wrapRootElement = ({ element }) => {
 //   return <RootElement>{element}</RootElement>;
