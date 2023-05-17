505f9349c7a0db960fd9efb71d73274facfc6aad
JP Harris <jpharris.contact@gmail.com>
2023-05-17 11:44:45 -0400

    updates: siteUrl property

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: `JP Harris`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://jpharris.io`,
   },
   plugins: [
     `gatsby-plugin-image`,
